bed863f68f016bb8cfdfb7b589c3a03b88c1d90a
thehobojoe <the.hobo.joe@gmail.com>
2024-04-28 03:01:21 -0500

    Handle factories, cleanup

--- luaui/Widgets/unit_dgun_stall_assist.lua
+++ luaui/Widgets/unit_dgun_stall_assist.lua
@@ -12,23 +12,30 @@ end
 
 
 local spGetUnitCurrentCommand = Spring.GetUnitCurrentCommand
+local spGetFactoryCommands = Spring.GetFactoryCommands
 local spGetUnitResources = Spring.GetUnitResources
+local spGetUnitDefID = Spring.GetUnitDefID
 local CMD_WAIT = CMD.WAIT
 
 
 local targetEnergy = 1000
-local unwaitThreshold = 2 -- how long to wait after dgun before unwaiting, in seconds
+local dgunCost = 500
+local unwaitThreshold = 3 -- how long to wait after dgun before unwaiting, in seconds
 local gameStarted = false
 local dgunActive = false
-local waitableUnits = {}
+
 local waitingUnits = {}
 local lastDgunTime = 0
 local timer = 0
 
-
+local waitableUnits = {}
+local isFactory = {}
 for uDefID, uDef in pairs(UnitDefs) do
 	if (uDef.buildSpeed > 0) and (uDef.canAssist or uDef.canResurrect or uDef.isFactory) and (not uDef.canManualFire) then
 		waitableUnits[uDefID] = true
+	end
+	if uDef.isFactory then
+		isFactory[uDefID] = true
 	end
 end
 
@@ -52,7 +59,26 @@ end
 
 local function doWeHaveLowEnergy()
 	local currentEnergy, energyStorage = Spring.GetTeamResources(Spring.GetMyTeamID(), "energy")
-	return currentEnergy < targetEnergy
+	if energyStorage < dgunCost then return false end
+	if currentEnergy < targetEnergy then return true end
+	return false
+end
+
+
+local function isWaiting(unitID)
+	local unitDefID = spGetUnitDefID(unitID)
+
+	local cmd
+	if isFactory[unitDefID] then -- factories need their own handling, fun!
+		local facCmds = spGetFactoryCommands(unitID, 1)
+		if facCmds and #facCmds > 0 then
+			cmd = facCmds[1].id
+		end
+	else
+		cmd = spGetUnitCurrentCommand(unitID, 1)
+	end
+
+	return cmd and cmd == CMD_WAIT
 end
 
 
@@ -60,8 +86,7 @@ local function waitUnits()
 	waitingUnits = getEnergyConsumingBuilders()
 	local toWait = {}
 	for i = 1, #waitingUnits do
-		local uCmd = spGetUnitCurrentCommand(waitingUnits[i], 1)
-		if uCmd ~= CMD_WAIT then
+		if not isWaiting(waitingUnits[i]) then
 			toWait[#toWait + 1] = waitingUnits[i]
 		end
 	end
@@ -72,9 +97,7 @@ end
 local function unwaitUnits()
 	local toUnwait = {}
 	for i = 1, #waitingUnits do
-		local uCmd = spGetUnitCurrentCommand(waitingUnits[i], 1)
-		Spring.Echo("ucmd", uCmd)
-		if not uCmd or uCmd == CMD_WAIT then
+		if isWaiting(waitingUnits[i]) then
 			toUnwait[#toUnwait + 1] = waitingUnits[i]
 		end
 	end
